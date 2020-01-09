d72bf5094cfcad48937636db41adb99354bf772c
Urban Attila <attilaurban94@gmail.com>
2020-01-09 13:54:28 +0100

    Rename

--- src/main/java/com/calendar/Calendar2k20.java
+++ src/main/java/com/calendar/Calendar2k20.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Calendar2k20Application {
+public class Calendar2k20 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Calendar2k20Application.class, args);
+		SpringApplication.run(Calendar2k20.class, args);
 	}
 
 }
--- src/test/java/com/calendar/Calendar2k20Tests.java
+++ src/test/java/com/calendar/Calendar2k20Tests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Calendar2k20ApplicationTests {
+class Calendar2k20Tests {
 
 	@Test
 	void contextLoads() {
