9ebfea1262e00b9d4f7c44ab2755b071b2a7f59e
Urban Attila <attilaurban94@gmail.com>
2020-01-09 13:48:13 +0100

    Initial commit

---
+++ src/main/java/com/calendar/Calendar2k20Application.java
@@ -0,0 +1,13 @@
+package com.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calendar2k20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calendar2k20Application.class, args);
+	}
+
+}
