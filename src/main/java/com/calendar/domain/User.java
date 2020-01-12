da0f52d26318014048a42f149af1acf70bdf3812
Urban Attila <attilaurban94@gmail.com>
2020-01-12 19:39:01 +0100

    User & RegistrationDto class

--- src/main/java/com/calendar/domain/User.java
+++ src/main/java/com/calendar/domain/User.java
@@ -1,7 +1,9 @@
 package com.calendar.domain;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,52 +15,104 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "tbl_user")
-public class User implements UserDetails{
-	
+public class User implements UserDetails {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	private LocalDateTime registrationTime;
+
+	private String validateToken;
+
+	private boolean isValidated;
+
+	private boolean isDeleted;
+
+	public User() {
+
+	}
+
+	public User(String email, String password, String validateToken) {
+		this.email = email;
+		this.password = password;
+		this.validateToken = validateToken;
+
+		isValidated = false;
+		isDeleted = false;
+	}
+
+	public boolean isValidated() {
+		return isValidated;
+	}
+
+	public void setValidated(boolean isValidated) {
+		this.isValidated = isValidated;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public LocalDateTime getRegistrationTime() {
+		return registrationTime;
+	}
+
+	public String getValidateToken() {
+		return validateToken;
+	}
+
+	public boolean isDeleted() {
+		return isDeleted;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return password;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return email;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return !isDeleted;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		return !isDeleted;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return !isDeleted;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return !isDeleted;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
 	}
 
 }
---
+++ src/main/java/com/calendar/requestdto/RegistrationDto.java
@@ -0,0 +1,44 @@
+package com.calendar.requestdto;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+public class RegistrationDto {
+	
+	private static final String EMAIL_REGEXP ="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:"
+			+ "[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")"
+			+ "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|"
+			+ "[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\"
+			+ "x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+	
+	@NotNull
+	@Pattern(regexp = EMAIL_REGEXP, message = "notValidEmail")
+	private String email;
+	
+	@NotNull
+	@Size(min = 6, max = 20, message = "notInRange")
+	private String password;
+
+	public RegistrationDto() {
+		
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
