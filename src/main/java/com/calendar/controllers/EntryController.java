234c4855f89798b0567e9db175a3d1594f80d2f8
Urban Attila <attilaurban94@gmail.com>
2020-01-18 19:58:11 +0100

    add getentries

--- src/main/java/com/calendar/controllers/EntryController.java
+++ src/main/java/com/calendar/controllers/EntryController.java
@@ -3,14 +3,18 @@ package com.calendar.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.calendar.requestdto.EntryDto;
+import com.calendar.responsedto.EntryResponseDto;
 import com.calendar.service.impl.EntryServiceImpl;
 
 @RestController
+@RequestMapping("/entry")
 public class EntryController {
 	
 	private EntryServiceImpl entryServiceImpl;
@@ -20,8 +24,14 @@ public class EntryController {
 		this.entryServiceImpl = entryServiceImpl;
 	}
 
-	@PostMapping("/makefirstentry")
+	@PostMapping("/firstentry")
 	public void makeFirstEntry(@Valid @RequestBody EntryDto entryDto) {
 		entryServiceImpl.makeFirstEntry(entryDto);
+	}
+	
+	@GetMapping("/getentries")
+	public EntryResponseDto getEntries() {
+		
+		return entryServiceImpl.getEntries();
 	}
 }
--- src/main/java/com/calendar/service/impl/EntryServiceImpl.java
+++ src/main/java/com/calendar/service/impl/EntryServiceImpl.java
@@ -10,16 +10,19 @@ import com.calendar.domain.Entry;
 import com.calendar.domain.User;
 import com.calendar.repository.EntryRepository;
 import com.calendar.requestdto.EntryDto;
+import com.calendar.responsedto.EntryResponseDto;
 import com.calendar.service.EntryService;
 
 @Service
 public class EntryServiceImpl implements EntryService {
 
 	private EntryRepository entryRepository;
+	private UserServiceImpl userServiceImpl;
 
 	@Autowired
-	public EntryServiceImpl(EntryRepository entryRepository) {
+	public EntryServiceImpl(EntryRepository entryRepository, UserServiceImpl userServiceImpl) {
 		this.entryRepository = entryRepository;
+		this.userServiceImpl = userServiceImpl;
 	}
 
 	@Override
@@ -29,10 +32,21 @@ public class EntryServiceImpl implements EntryService {
 		
 		Entry entry = new Entry(entryDto.getTitle(), entryDto.getDescription(), entryDto.getDate(), entryDto.getDuration(), entryDto.getTermin(), 
 				EntryType.valueOf(entryDto.getEntryType()) , EntryPhase.valueOf(entryDto.getEntryPhase()), 1);
-		entry.setUser(user);
+		entry.setUserId(user.getId());
 		user.addEntry(entry);
 		
 		entryRepository.save(entry);
 	}
 
+	public EntryResponseDto getEntries() {
+		
+		EntryResponseDto entryResponseDto = new EntryResponseDto();
+		
+		User user = userServiceImpl.getFullUser();
+		
+		entryResponseDto.setEntryList(user.getEntryList());
+		
+		return entryResponseDto;
+	}
+	
 }
