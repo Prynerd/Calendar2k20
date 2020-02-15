7e005684200f1709656c6ec350d041a01a3e870f
Urban Attila <attilaurban94@gmail.com>
2020-02-15 20:26:07 +0100

    getFullProjektById

---
+++ src/main/java/com/calendar/responsedto/FullProjectResponseDto.java
@@ -0,0 +1,158 @@
+package com.calendar.responsedto;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import com.calendar.data.enums.EntryPhase;
+import com.calendar.data.enums.EntryType;
+import com.calendar.domain.Entry;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class FullProjectResponseDto {
+
+	private int id;
+	
+	private int userId;
+	
+	private String title;
+
+	private String description;
+
+	@JsonFormat(pattern = "yyyy-MM-dd@HH:mm")
+	private LocalDateTime date;
+
+	@JsonFormat(pattern = "yyyy-MM-dd@HH:mm")
+	private LocalDateTime duration;
+
+	@JsonFormat(pattern = "yyyy-MM-dd@HH:mm")
+	private LocalDateTime termin;
+
+	@Enumerated(EnumType.STRING)
+	private EntryType entryType;
+
+	@Enumerated(EnumType.STRING)
+	private EntryPhase entryPhase;
+	
+	private boolean isChild;
+	
+	private boolean isFinished;
+	
+	private Set<Entry> addEntry = new HashSet<Entry>();
+	
+	public FullProjectResponseDto(int id, int userId, String title, String description, LocalDateTime date, LocalDateTime duration,
+			LocalDateTime termin, EntryType entryType, EntryPhase entryPhase, boolean isChild, boolean isFinished, Set<Entry> addEntry) {
+		this.id = id;
+		this.userId = userId;
+		this.title = title;
+		this.description = description;
+		this.date = date;
+		this.duration = duration;
+		this.termin = termin;
+		this.entryType = entryType;
+		this.entryPhase = entryPhase;
+		this.isChild = isChild;
+		this.isFinished = isFinished;
+		this.addEntry = addEntry;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public LocalDateTime getDuration() {
+		return duration;
+	}
+
+	public void setDuration(LocalDateTime duration) {
+		this.duration = duration;
+	}
+
+	public LocalDateTime getTermin() {
+		return termin;
+	}
+
+	public void setTermin(LocalDateTime termin) {
+		this.termin = termin;
+	}
+
+	public EntryType getEntryType() {
+		return entryType;
+	}
+
+	public void setEntryType(EntryType entryType) {
+		this.entryType = entryType;
+	}
+
+	public EntryPhase getEntryPhase() {
+		return entryPhase;
+	}
+
+	public void setEntryPhase(EntryPhase entryPhase) {
+		this.entryPhase = entryPhase;
+	}
+
+	public boolean isChild() {
+		return isChild;
+	}
+
+	public void setChild(boolean isChild) {
+		this.isChild = isChild;
+	}
+
+	public boolean isFinished() {
+		return isFinished;
+	}
+
+	public void setFinished(boolean isFinished) {
+		this.isFinished = isFinished;
+	}
+	
+	public Set<Entry> getAddEntry() {
+		return addEntry;
+	}
+
+	public void AddEntry(Set<Entry> addEntry) {
+		this.addEntry = addEntry;
+	}
+	
+}
