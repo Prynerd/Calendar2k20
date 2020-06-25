eee4ca39463035acaba4a84f3873c8cb4c9cf1ed
danvarga1985 <danvarga1985@gmail.com>
2020-06-25 19:13:56 +0200

    Added PostgreSQL Connection

--- src/main/java/com/calendar/dao/EntryDao.java
+++ src/main/java/com/calendar/dao/EntryDao.java
@@ -3,19 +3,14 @@ package com.calendar.dao;
 import com.calendar.Connection.DBConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class EntryDao {
 
     private SQLQuery sqlQuery;
     private DBConnection dbConnection;
-
-    public EntryDao() {
-    }
 
     @Autowired
     public EntryDao(SQLQuery sqlQuery, DBConnection dbConnection) {
@@ -24,13 +19,13 @@ public class EntryDao {
     }
 
 
-    public List<Integer> getParents(int entryId) throws SQLException {
+    public ArrayList<Integer> getParents(int entryId) throws SQLException {
 
-        List<Integer> resultList = new ArrayList<>();
+        ArrayList<Integer> resultList = new ArrayList<>();
 
         try (Connection connection = DriverManager
-//                .getConnection("localhost/calendar2k20", "root", "The@peOfNaples01");
-             .getConnection(dbConnection.getUrl(), dbConnection.getUser(), dbConnection.getPassword());
+                .getConnection(dbConnection.getUrl(), dbConnection.getUser(), dbConnection.getPassword());
+
              PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery.getQuery());) {
 
             preparedStatement.setInt(1, entryId);
