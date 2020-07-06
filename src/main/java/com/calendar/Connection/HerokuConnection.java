a603a4145aeaab42ab79bfa307564cf3cc7abf49
danvarga1985 <danvarga1985@gmail.com>
2020-07-06 16:38:43 +0200

    deploy

--- src/main/java/com/calendar/Connection/HerokuConnection.java
+++ src/main/java/com/calendar/Connection/HerokuConnection.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class HerokuConnection extends DBConnection {
 
     public HerokuConnection() {
-        this.url ="jdbc:postgres://ec2-54-228-250-82.eu-west-1.compute.amazonaws.com:5432/d6981inv0knfbm";
+        this.url ="jdbc:postgresql://ec2-54-228-250-82.eu-west-1.compute.amazonaws.com:5432/d6981inv0knfbm";
         this.user = "aywfgexrcvohxc";
         this.password = "a31ceef08451da1c37651478c56b94711b57f140c7c553770a4c1a065aa9c29c";
     }
