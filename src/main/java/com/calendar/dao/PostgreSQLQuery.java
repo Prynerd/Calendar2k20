adc720a817804176440246ed26d4a66688a1ec88
danvarga1985 <danvarga1985@gmail.com>
2020-07-06 16:53:56 +0200

    deploy

--- src/main/java/com/calendar/dao/PostgreSQLQuery.java
+++ src/main/java/com/calendar/dao/PostgreSQLQuery.java
@@ -9,21 +9,21 @@ public class PostgreSQLQuery extends SQLQuery {
 
     public PostgreSQLQuery() {
         super.setQuery("WITH RECURSIVE entries AS(\n" +
-                "    SELECT\n" +
-                "        id,\n" +
-                "        entry_id\n" +
-                "    FROM\n" +
-                "        entry\n" +
-                "    WHERE\n" +
-                "        id = 5\n" +
-                "    UNION ALL\n" +
-                "        SELECT\n" +
-                "            e.id,\n" +
-                "            e.entry_id\n" +
-                "        FROM\n" +
-                "             entry e\n" +
-                "        INNER JOIN entries entr ON entr.entry_id = e.id\n" +
-                ")\n" +
-                "SELECT id FROM entries WHERE entry_id is null;");
+                "                    SELECT\n" +
+                "                        id,\n" +
+                "                         entry_id\n" +
+                "                    FROM\n" +
+                "                        entry\n" +
+                "                    WHERE\n" +
+                "                        id = 11\n" +
+                "                    UNION ALL\n" +
+                "                        SELECT\n" +
+                "                            e.id,\n" +
+                "                            e.entry_id\n" +
+                "                           FROM\n" +
+                "                             entry e\n" +
+                "                        INNER JOIN entries entr ON entr.entry_id = e.id\n" +
+                "                )\n" +
+                "                SELECT id FROM entries WHERE entry_id is null;");
     }
 }
