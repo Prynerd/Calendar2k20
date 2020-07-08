1628c92ebe90056c75b4b0f7f083e8e63adf7a1e
danvarga1985 <danvarga1985@gmail.com>
2020-07-08 14:18:09 +0200

    Fixing NullPointer Error in deleteEntryById

--- src/main/java/com/calendar/service/impl/EntryServiceImpl.java
+++ src/main/java/com/calendar/service/impl/EntryServiceImpl.java
@@ -152,12 +152,10 @@ public class EntryServiceImpl implements EntryService {
 
 		User user = userServiceImpl.getFullUser();
 
-		int projectId = getProjectIdOfEntry(id);
-
 		if(id != null) {
-			return new ProjectViewResponseDto(getFullProjectById(projectId), getProjects(user.isOnlyActiveProjects()));
+			return new ProjectViewResponseDto(getFullProjectById(getProjectIdOfEntry(id)), getProjects(user.isOnlyActiveProjects()));
 		} else {
-			return new ProjectViewResponseDto(null,getProjects(user.isOnlyActiveProjects()));
+			return new ProjectViewResponseDto(null, getProjects(user.isOnlyActiveProjects()));
 		}
 
 	}
@@ -189,7 +187,6 @@ public class EntryServiceImpl implements EntryService {
 		checkUserToEntry(e);
 
 		customEntryRepository.removeEntry(e);
-
 
 		try {
 			int parentId = e.getEntryConnections().getId();
