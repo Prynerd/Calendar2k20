1a476f6d5e1b555622de568487ff7783f3ce273c
danvarga1985 <danvarga1985@gmail.com>
2020-07-28 09:49:48 +0200

    fix

--- src/main/java/com/calendar/config/WebSecConfig.java
+++ src/main/java/com/calendar/config/WebSecConfig.java
@@ -67,7 +67,7 @@ public class WebSecConfig extends WebSecurityConfigurerAdapter {
 	    		.permitAll()
 	    		.invalidateHttpSession(true)
 	    		.deleteCookies("JSESSIONID")
-	    		.logoutSuccessUrl("/login-registration")
+	    		.logoutSuccessUrl("https://google.com")
 	    		;
 	}
 	
