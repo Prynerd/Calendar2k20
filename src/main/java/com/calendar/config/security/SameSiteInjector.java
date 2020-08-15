ff1acc3c2a5a4320cd9510299d987eabb6f85e8a
danvarga1985 <danvarga1985@gmail.com>
2020-08-15 20:59:09 +0200

    cookie fix1

---
+++ src/main/java/com/calendar/config/security/SameSiteInjector.java
@@ -0,0 +1,24 @@
+package com.calendar.config.security;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SameSiteInjector {
+
+    private final ApplicationContext applicationContext;
+
+    public SameSiteInjector(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @EventListener
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        DefaultCookieSerializer cookieSerializer = applicationContext.getBean(DefaultCookieSerializer.class);
+        cookieSerializer.setUseSecureCookie(true);
+    }
+}
