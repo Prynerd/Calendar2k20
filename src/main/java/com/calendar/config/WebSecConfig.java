c0adfe9a461cd2b8c087de91eb95987ce1060d4c
danvarga1985 <danvarga1985@gmail.com>
2020-08-16 12:22:37 +0200

    CookieServiceInterceptor

--- src/main/java/com/calendar/config/WebSecConfig.java
+++ src/main/java/com/calendar/config/WebSecConfig.java
@@ -13,8 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -103,10 +101,5 @@ public class WebSecConfig extends WebSecurityConfigurerAdapter {
         return source;
     }
 
-	@Bean
-	public CookieSerializer cookieSerializer() {
-		DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-		serializer.setSameSite("none");
-		return serializer;
-	}
+
 }
