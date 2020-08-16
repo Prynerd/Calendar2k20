01e9f0609346772a74bae134295a9c4a368f2716
danvarga1985 <danvarga1985@gmail.com>
2020-08-16 12:32:23 +0200

    MvcCOnfing

--- src/main/java/com/calendar/config/security/CookieServiceInterceptor.java
+++ src/main/java/com/calendar/config/security/CookieServiceInterceptor.java
@@ -1,54 +1,54 @@
-package com.calendar.config.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.servlet.handler.MappedInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
-
-@Component
-public class CookieServiceInterceptor extends HandlerInterceptorAdapter {
-
-    @Autowired
-    CookieServiceInterceptor cookieServiceInterceptor;
-
-    @Bean
-    public MappedInterceptor myInterceptor()
-    {
-        return new MappedInterceptor(null, cookieServiceInterceptor);
-    }
-
-    @Override
-    public boolean preHandle(
-            HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        return true;
-    }
-
-    @Override
-    public void postHandle(
-            HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception {
-        //check whether it has "set-cookie" in the response, if it has, then add "SameSite" attribute
-        //it should be found in the response of the first successful login
-        Collection<String> headers = response.getHeaders(HttpHeaders.SET_COOKIE);
-        boolean firstHeader = true;
-        for (String header : headers) { // there can be multiple Set-Cookie attributes
-            if (firstHeader) {
-                response.setHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s",  header, "SameSite=strict"));
-                firstHeader = false;
-                continue;
-            }
-            response.addHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s",  header, "SameSite=strict"));
-        }
-    }
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-                                Object handler, Exception exception) throws Exception {}
-}
+//package com.calendar.config.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+//import org.springframework.web.servlet.handler.MappedInterceptor;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.Collection;
+//
+//@Component
+//public class CookieServiceInterceptor extends HandlerInterceptorAdapter {
+//
+//    @Autowired
+//    CookieServiceInterceptor cookieServiceInterceptor;
+//
+//    @Bean
+//    public MappedInterceptor myInterceptor()
+//    {
+//        return new MappedInterceptor(null, cookieServiceInterceptor);
+//    }
+//
+//    @Override
+//    public boolean preHandle(
+//            HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//
+//        return true;
+//    }
+//
+//    @Override
+//    public void postHandle(
+//            HttpServletRequest request, HttpServletResponse response, Object handler,
+//            ModelAndView modelAndView) throws Exception {
+//        //check whether it has "set-cookie" in the response, if it has, then add "SameSite" attribute
+//        //it should be found in the response of the first successful login
+//        Collection<String> headers = response.getHeaders(HttpHeaders.SET_COOKIE);
+//        boolean firstHeader = true;
+//        for (String header : headers) { // there can be multiple Set-Cookie attributes
+//            if (firstHeader) {
+//                response.setHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s",  header, "SameSite=strict"));
+//                firstHeader = false;
+//                continue;
+//            }
+//            response.addHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s",  header, "SameSite=strict"));
+//        }
+//    }
+//    @Override
+//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+//                                Object handler, Exception exception) throws Exception {}
+//}
---
+++ src/main/java/com/calendar/config/security/CookiesInterceptor.java
@@ -0,0 +1,11 @@
+//@Configuration
+//public class MvcConfiguration implements WebMvcConfigurer {
+//
+//    @Bean
+//    public TomcatContextCustomizer sameSiteCookiesConfig() {
+//        return context -> {
+//            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+//            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+//            context.setCookieProcessor(cookieProcessor);
+//        };
+//    }
