7d4239002ca91bb7a3e3ad5be016582c66dfb75e
danvarga1985 <danvarga1985@gmail.com>
2020-10-01 14:05:15 +0200

    SQLQuery fix

--- src/main/java/com/calendar/dao/PostgreSQLQuery.java
+++ src/main/java/com/calendar/dao/PostgreSQLQuery.java
@@ -11,7 +11,7 @@ public class PostgreSQLQuery extends SQLQuery {
         super.setQuery("WITH RECURSIVE entries AS(\n" +
                 "    SELECT\n" +
                 "        id,\n" +
-                "        entry_id\n" +
+                "        parent_id\n" +
                 "    FROM\n" +
                 "        entry\n" +
                 "    WHERE\n" +
@@ -19,11 +19,11 @@ public class PostgreSQLQuery extends SQLQuery {
                 "    UNION ALL\n" +
                 "        SELECT\n" +
                 "            e.id,\n" +
-                "            e.entry_id\n" +
+                "            e.parent_id\n" +
                 "        FROM\n" +
                 "             entry e\n" +
-                "        INNER JOIN entries entr ON entr.entry_id = e.id\n" +
+                "        INNER JOIN entries entr ON entr.parent_id = e.id\n" +
                 ")\n" +
-                "SELECT id FROM entries WHERE entry_id is null;");
+                "SELECT id FROM entries WHERE parent_id is null;");
     }
 }
